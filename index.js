80dd5eebd326fb00d0b8700e50d4f877d505d9a2
mayukuse24 <vinaypoll@gmail.com>
2020-01-22 13:47:22 -0600

    Get range from POST body. Updated geocoder to look only for locations in Australia

--- index.js
+++ index.js
@@ -15,19 +15,20 @@ app.get('/', (req, res) => res.send('Welcome to points to map!'));
 
 app.post('/', (req, res) => {
     var address,
-        reqBody = _.get(req, 'body');;
+        reqBody = _.get(req, 'body');
     
     // TODO: validate request
     console.log(req.body);
     
-    if (!reqBody) { return res.status(400).send('No body in request'); };
+    if (!reqBody) { return res.status(400).send('No body in request'); }
 
     address = _.get(reqBody, 'address');
+    range = _.get(reqBody, 'range') || 10;
     
     if (!address) { return res.status(400).send('Address not provided or incorrect request format'); }
     
     // Convert user provided address to latitude, longitude
-    geocoder.geocode({ 'address': address }, function (err, geolocations) {
+    geocoder.geocode({ 'address': address, countryCode: 'AU' }, function (err, geolocations) {
         if (err) { 
             console.log('geocode error', err);
             return res.status(400).send(err); 
@@ -40,7 +41,7 @@ app.post('/', (req, res) => {
         }
     
         // Query elasticsearch to retrieve locations in range
-        esInterface.getLocationsInRange(point, 10, function (err, locations) {
+        esInterface.getLocationsInRange(point, range, function (err, locations) {
             if (err) { return res.status(404).send(err); } // TODO: better error handling/status code
     
             // Send response to frontend for displaying points
