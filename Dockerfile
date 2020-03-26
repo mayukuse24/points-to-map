ce6422fbfc3d8645370965b780d3025f969b6904
mayukuse24 <vinaypoll@gmail.com>
2020-03-26 15:08:17 -0500

    Removing duplicates

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM node:10.17.0-alpine
-WORKDIR /app
-COPY package.json /app
-RUN npm install
-COPY . /app
-EXPOSE 3000
-CMD ["node", "index.js"]
--- config.js
+++
@@ -1,6 +0,0 @@
-// TODO: fix config format and multiple dev environments
-module.exports = {
-    port: process.env.SERVER_PORT,
-    esDomain: process.env.ES_DOMAIN,
-    openCageKey: process.env.OPEN_CAGE_KEY
-};
